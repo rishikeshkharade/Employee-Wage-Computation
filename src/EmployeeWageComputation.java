248d71620ee1b944dc7f3968b15cc4561001df08
RISHIKESH KHARADE <rishikeshkharade19@gmail.com>
2024-11-26 14:04:14 +0530

    Added: UC3 Part Time Employee & Wage

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -2,20 +2,27 @@ public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.print("Welcome to Employee Wage Computation Program");
         final int FULL_TIME = 1;
+        int PART_TIME = 2;
         final int WAGE_PER_HR=20;
-        int workingHrs;
-        int empType = (int) (Math.random() * 100) % 2;
+        int workingHrs = 0;
+        int empType = (int) (Math.random() * 100) % 3
+                ;
         if (empType == FULL_TIME)
         {
             System.out.println("Employee is Present");
             workingHrs=8;
+        }
+        else if(empType == PART_TIME){
+            System.out.println("Employee is Present Part Time");
+            workingHrs=4;
         }
         else
         {
             System.out.println("Employee is Absent");
         }
         int wage= workingHrs * WAGE_PER_HR;
-        
+        System.out.println("Employee Daily Wage is " + wage);
+
 
     }
 }
