03cbc37df284fd8ca1b222e5075436e854e1f519
RISHIKESH KHARADE <rishikeshkharade19@gmail.com>
2024-11-26 15:26:24 +0530

    UC5: Calculated wages for a month

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -1,29 +1,34 @@
+import java.sql.SQLOutput;
+
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.print("Welcome to Employee Wage Computation Program");
         final int FULL_TIME = 1;
         final int PART_TIME = 2;
-        final int WAGE_PER_HR=20;
-        int workingHrs = 0;
-        int empType = (int) (Math.random() * 100) %3;
+        final int WAGE_PER_HR = 20;
+        final int WORKING_DAYS = 20;
+        int total_wage = 0;
+        for (int day = 1; day <= WORKING_DAYS; day++) {
+            int empType = (int) (Math.random() * 100) % 3;
+            int workingHrs = 0;
+            switch (empType) {
+                case FULL_TIME:
+                    System.out.println("Employee is Present");
+                    workingHrs = 8;
+                    break;
 
-        switch(empType) {
-            case FULL_TIME:
-            System.out.println("Employee is Present");
-            workingHrs=8;
-            break;
+                case PART_TIME:
+                    System.out.println("Employee is Present Part Time");
+                    workingHrs = 4;
+                    break;
 
-            case PART_TIME:
-            System.out.println("Employee is Present Part Time");
-            workingHrs=4;
-            break;
-
-            default:
-            System.out.println("Employee is Absent");
+                default:
+                    System.out.println("Employee is Absent");
+            }
+            int wage = workingHrs * WAGE_PER_HR;
+            System.out.println("Employee Daily Wage is " + wage);
+            total_wage += wage;
         }
-        int wage= workingHrs * WAGE_PER_HR;
-        System.out.println("Employee Daily Wage is " + wage);
-
-
+        System.out.println("Total wage for a month is " + total_wage);
     }
 }
