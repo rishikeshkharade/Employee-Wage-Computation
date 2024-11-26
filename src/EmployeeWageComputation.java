d420cf8cbb682e23dc0472ca2f7b1813f341679c
RISHIKESH KHARADE <rishikeshkharade19@gmail.com>
2024-11-26 15:02:17 +0530

    Switch Statement

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -2,22 +2,23 @@ public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.print("Welcome to Employee Wage Computation Program");
         final int FULL_TIME = 1;
-        int PART_TIME = 2;
+        final int PART_TIME = 2;
         final int WAGE_PER_HR=20;
         int workingHrs = 0;
-        int empType = (int) (Math.random() * 100) % 3
-                ;
-        if (empType == FULL_TIME)
-        {
+        int empType = (int) (Math.random() * 100) %3;
+
+        switch(empType) {
+            case FULL_TIME:
             System.out.println("Employee is Present");
             workingHrs=8;
-        }
-        else if(empType == PART_TIME){
+            break;
+
+            case PART_TIME:
             System.out.println("Employee is Present Part Time");
             workingHrs=4;
-        }
-        else
-        {
+            break;
+
+            default:
             System.out.println("Employee is Absent");
         }
         int wage= workingHrs * WAGE_PER_HR;
