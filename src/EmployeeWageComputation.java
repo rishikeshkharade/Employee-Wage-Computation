c1581bce4d8bfc74ca675239fb13bce9ca27bcca
RISHIKESH KHARADE <rishikeshkharade19@gmail.com>
2024-11-29 14:00:51 +0530

    UC7 Refactored code to class method (Compute Employee Wage)

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -1,16 +1,19 @@
-import java.sql.SQLOutput;
-
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
-        final int FULL_TIME = 1;
-        final int PART_TIME = 2;
-        final int WAGE_PER_HR = 20;
-        final int MAX_WORKING_DAYS = 20;
-        final int MAX_WORKING_HRS = 100;
+        computeEmployeewage();
+}
+
+    static final int FULL_TIME = 1;
+    static final int PART_TIME = 2;
+    static final int WAGE_PER_HR = 20;
+    static final int MAX_WORKING_DAYS = 20;
+    static final int MAX_WORKING_HRS = 100;
+
+    public static void computeEmployeewage() {
         int total_wage = 0;
         int workingHrs = 0;
-        for (int day = 1,totalworkingHrs=0; day<= MAX_WORKING_DAYS && totalworkingHrs < MAX_WORKING_HRS; day++) {
+        for (int day = 1, totalworkingHrs = 0; day <= MAX_WORKING_DAYS && totalworkingHrs < MAX_WORKING_HRS; day++) {
             int empType = (int) (Math.random() * 100) % 3;
 
             switch (empType) {
@@ -31,8 +34,8 @@ public class EmployeeWageComputation {
             int wage = workingHrs * WAGE_PER_HR;
             System.out.println("Employee Daily Wage is " + wage);
             total_wage += wage;
-            totalworkingHrs+=workingHrs;
-            System.out.println("Day= "+day+", WorkingHrs= "+workingHrs+", Wage= "+wage+", Total Working Hrs= "+totalworkingHrs);
+            totalworkingHrs += workingHrs;
+            System.out.println("Day= " + day + ", WorkingHrs= " + workingHrs + ", Wage= " + wage + ", Total Working Hrs= " + totalworkingHrs);
         }
         System.out.println("Total wage for a month is " + total_wage);
     }
