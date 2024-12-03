20ecf0f1691a6b566ffd5262428b93e47da5c808
RISHIKESH KHARADE <rishikeshkharade19@gmail.com>
2024-12-03 11:13:51 +0530

    UC8 Computed Employee Wage for multiple companies

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -1,18 +1,19 @@
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
-        computeEmployeewage();
+        computeEmployeewage("Company QK",20,20,100);
+        computeEmployeewage("Company Wipro",25,22,120);
+        computeEmployeewage("Company Infosys",18,25,110);
 }
 
     static final int FULL_TIME = 1;
     static final int PART_TIME = 2;
-    static final int WAGE_PER_HR = 20;
-    static final int MAX_WORKING_DAYS = 20;
-    static final int MAX_WORKING_HRS = 100;
 
-    public static void computeEmployeewage() {
+    public static void computeEmployeewage(String companyName, int WAGE_PER_HR, int MAX_WORKING_DAYS, int MAX_WORKING_HRS) {
         int total_wage = 0;
         int workingHrs = 0;
+
+        System.out.println("\nComputing wages for "+companyName);
         for (int day = 1, totalworkingHrs = 0; day <= MAX_WORKING_DAYS && totalworkingHrs < MAX_WORKING_HRS; day++) {
             int empType = (int) (Math.random() * 100) % 3;
 
